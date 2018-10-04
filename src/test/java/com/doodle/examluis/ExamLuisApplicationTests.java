6d1f47bc3ef7ee946149be85b839bcadfabc3ca2
luis.jimenez2 <luis.jimenez2@wnco.com>
2018-10-04 18:24:01 -0500

    starting from start.spring.io

---
+++ src/test/java/com/doodle/examluis/ExamLuisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.doodle.examluis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamLuisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
